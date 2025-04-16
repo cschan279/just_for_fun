de0498f7d95c1c57377705bc50c3523f3569bdcf
TroyUbuntu <troychan279@gmail.com>
2025-04-16 21:15:18 +0800

    first build

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,14 @@
 ﻿namespace just_for_fun.ViewModels;
 
+
+/*
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
+}
+*/
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia! This is my added text.";
 }
